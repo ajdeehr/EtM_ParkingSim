77f961d2e20eda3205b9ac64ed494e2534fa69e3
ajdeehr <46286580+ajdeehr@users.noreply.github.com>
2019-05-24 11:33:17 -0700

    Created Agent and Vehicle
    I created Agent and Vehicle, and updated Model so that it begins to create agents and vehicles
    Ignore the other branch

--- Model.py
+++ Model.py
@@ -1,9 +1,22 @@
-
-
 import numpy as N
 
+import Agent
+import Vehicle
 import Road
 import Garage
+
+STUDENT = 0
+FACULTY = 1
+STAFF = 2
+
+STANDARD = 0
+HANDICAP = 1
+CARPOOL = 2
+
+PARKED = 0
+MOVING = 1
+
+limit = 30
 
 class Model(obj):
 
@@ -20,7 +33,42 @@ class Model(obj):
         self.southGarage.outsideRoad = self.campusWayRoad
 
         self.numberGarage = 2
+        
+        self.agents_list = self.agent_init()
 
-        self.vehicleList = makeVehicleList()
+        self.vehicleList = self.makeVehicleList(self.agents_list)
+        
 
 
+    def agent_init(self):
+        credits = N.zeros((0,), dtype='i')
+        while (N.size(credits) < limit):
+            a = N.random.normal(15,1.5, 10).astype('i')
+            temp = a[N.where(N.logical_and(a > 0, a < 20))]
+            credits = N.concatenate([temp, credits])
+        
+        agents = N.ndarray((limit,), dtype=Agent.Agent)
+        for i in range(limit):
+            # print("Student {:2}: Credits {:2}".format(i + 1, credits[i]))
+            agents[i] = Agent.Agent(agenttype=STUDENT,creditshours=credits[i], \
+                            stayhours = (credits[i]//5) * 2, \
+                            agent_id = i, ta = (8,30) )
+        
+        return agents
+      
+    def makeVehicleList(self, list_agents):
+        vehicles = []
+        ite = 0
+        while ite < limit:
+            if N.random.random() > .95:
+                vehicles.append(Vehicle.Vehicle(STANDARD, \
+                            [list_agents[ite], list_agents[ite+1]], MOVING))
+                ite = ite + 1
+            else:
+                vehicles.append(Vehicle.Vehicle(STANDARD, \
+                            [list_agents[ite]], MOVING))
+            ite = ite + 1
+    
+        return w
+        # for i in range(len(vehicles)):
+        #     print(vehicles[i])
