318dcbc181e2a80db4e392d5304de157d5f44173
xavcheng <45876119+xavcheng@users.noreply.github.com>
2019-06-03 02:46:58 -0700

    Update Constants.py

--- Constants.py
+++ Constants.py
@@ -1,7 +1,38 @@
+#Student data from PDF
+FTE_2016 = 8,217  # Full time student Fall 2016
+FTE_2018 = 7,731  # Full time student Fall 2018
+FTE_AVG = int((FTE_2016 + FTE_2018)/2)
+
+# Student who drives along
+DRIVE_ALONE_16 = 0.579
+DRIVE_ALONE_18 = 0.539
+DRIVE_ALONE_AVG = (DRIVE_ALONE_16 + DRIVE_ALONE_18)/2
+
+# Student who carpool
+CARPOOL_16 = 0.136  #
+CARPOOL_18 = 0.088
+CARPOOL_AVG = (CARPOOL_16 + CARPOOL_18)/2
+
+#Total number of student car
+TOTAL_STUDENT_CAR = int(FTE_AVG * (DRIVE_ALONE_AVG + CARPOOL_AVG))
+
+# Other personnel
+STAFF_16 = 329
+STAFF_18 = 371
+STAFF_AVG = int((intSTAFF_16 + STAFF_16)/2)
+
+FACULTY_16 = 360
+FACULTY_18 = 357
+FACULTY_AVG = int((FACULTY_16 + FACULTY_16) /2)
+
+TOTAL_CAR = TOTAL_STUDENT_CAR + FACULTY_AVG + STAFF_AVG
+
 #Variables For the Vehicle Class.
 PERCENT_BIKE = 0.05      #The probablitiy of generating bike instead of a car.
 PERCENT_HCAP = 0.05      #Probability of the Car to belong to a disabled person.
-PERCENT_CARPOOL = 0.05   #Probability of the Car having more than One passenger.
+#PERCENT_CARPOOL = 0.05  # Probability of the Car having more than One passenger.
+#(8217+7731)/2 = 7974, (0.136+0.088)/2 = 0.113
+PERCENT_CARPOOL = 0.113  # Probability of the Car having more than One passenger.
 
 VEHICLE_TYPE_CAR = 0     #Represent the types of the vehicle.
 VEHICLE_TYPE_BIKE = 1
@@ -27,3 +58,4 @@ PLUS_MINUS = 30
 
 
 #Other Variables Go here.
+
