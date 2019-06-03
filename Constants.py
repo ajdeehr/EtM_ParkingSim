26b538251f7d233287031e7b690587778ea8d898
xavcheng <45876119+xavcheng@users.noreply.github.com>
2019-06-03 02:36:11 -0700

    Update Constants.py

--- Constants.py
+++ Constants.py
@@ -1,7 +1,27 @@
+#Student data from PDF
+FTE2016 = 8,217  # Full time student Fall 2016
+FTE2018 = 7,731  # Full time student Fall 2018
+FTE_AVG = int((FTE2016 + FTE2018)/2)
+
+# Student who drives along
+DRIVE_ALONE_16 = 0.579
+DRIVE_ALONE_18 = 0.539
+DRIVE_ALONE_AVG = (DRIVE_ALONE_16 + DRIVE_ALONE_18)/2
+
+# Student who carpool
+CARPOOL_16 = 0.136  #
+CARPOOL_18 = 0.088
+CARPOOL_AVG = (CARPOOL_16 + CARPOOL_18)/2
+
+#Total number of car
+TOTAL_CAR = INT(FTE_AVG * (DRIVE_ALONE_AVG + CARPOOL_AVG))
+
 # Variables For the Vehicle Class.
 PERCENT_BIKE = 0.05  # The probablitiy of generating bike instead of a car.
 PERCENT_HCAP = 0.05  # Probability of the Car to belong to a disabled person.
-PERCENT_CARPOOL = 0.05  # Probability of the Car having more than One passenger.
+#PERCENT_CARPOOL = 0.05  # Probability of the Car having more than One passenger.
+#(8217+7731)/2 = 7974, (0.136+0.088)/2 = 0.113
+PERCENT_CARPOOL = 0.113  # Probability of the Car having more than One passenger.
 
 VEHICLE_TYPE_CAR = 0  # Represent the types of the vehicle.
 VEHICLE_TYPE_BIKE = 1
